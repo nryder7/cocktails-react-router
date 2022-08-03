a2ce01f6a7b3a414742de7624d39feaa9f51c079
Nicholas Ryder <nryder7@gmail.com>
2022-08-03 10:41:23 -0400

    fix name in cocktailjs

--- src/components/Cocktail.js
+++ src/components/Cocktail.js
@@ -2,15 +2,15 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { useGlobalContext } from '../context';
 
-const Cocktail = ({ drinkThumb, name, id, info, glass }) => {
+const Cocktail = ({ drinkThumb, drink, id, info, glass }) => {
   const { setSingleCocktail } = useGlobalContext();
   return (
     <article id={id} className='cocktail'>
       <div className='img-container'>
-        <img src={drinkThumb} alt={name} />
+        <img src={drinkThumb} alt={drink} />
       </div>
       <div className='cocktail-footer'>
-        <h3>{name}</h3>
+        <h3>{drink}</h3>
         <h4>{glass}</h4>
         <p>{info}</p>
         <Link to={`/cocktail/${id}`} className='btn btn-primary btn-details'>
